366c60c436501978ca090fc5998d2e294a526dec
lucian.ariseanu <lucian.ariseanu@yahoo.com>
2025-01-30 21:46:35 +0200

    Changed button program

--- 06_button/main.c
+++ 06_button/main.c
@@ -1,7 +1,10 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-#define LED    PB0
+#define LED1    PB0
+#define LED2    PB1
+#define LED3    PB2
+
 #define BUTTON PD7
 
 
@@ -18,20 +21,27 @@ int main() {
 	/* Set PD7 to input (~1) */
 	DDRD &= ~(1<<PD7); 
 
+	uint8_t a[3];
+	a[0] = PB0;
+	a[1] = PB1;
+	a[2] = PB2;
+	int counter=0;
 	while(1)
 	{
 		if ((PIND & (1<<BUTTON)) > 0) 
-		{
+		{	
 			_delay_ms(50); /* Debouncing */
 			if(state == 0) {
-				PORTB ^= (1<<LED);
+				PORTB ^= (1<<a[counter]);
+				counter++;
+				counter %= 3;
 				state = 1;
 			}
 			else {
 				state = 0;
 			}
 		}
-
 	}
 	return 0;
 }
+
